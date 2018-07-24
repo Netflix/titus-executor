f8e12f037ffbe6c2a2c5ee9c98c32bb4f35a27f4
Sargun Dhillon <sargun@sargun.me>
2018-07-24 13:57:12 -0700

    Fix LXCFS

--- executor/runtime/docker/lxcfs.go
+++ executor/runtime/docker/lxcfs.go
@@ -15,7 +15,7 @@ func getlxcfsbindmounts() []string {
 	extraBinds := []string{}
 	for _, file := range []string{"cpuinfo", "meminfo", "uptime"} {
 		path := filepath.Join(lxcfs, "proc", file)
-		if err := unix.Access(path, 0444); err != nil {
+		if err := unix.Access(path, 0); err == nil {
 			destPath := filepath.Join("/proc", file)
 			extraBinds = append(extraBinds, fmt.Sprintf("%s:%s", path, destPath))
 		}
