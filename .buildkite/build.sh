93d1ba2e74c1a533709390f18547abecd7317806
Rob Gulewich <rgulewich@netflix.com>
2018-09-19 16:39:14 -0700

    Add log lines to make debugging CI easier

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -uex -o pipefail
 
+log() {
+    echo -e "[$(date -u +"%Y-%m-%dT%H:%M:%SZ")] $1" >&2
+}
+
 # This should clean the path
 mkdir -p ${BUILDKITE_BUILD_CHECKOUT_PATH}/src/github.com/Netflix/titus-executor/
 
@@ -12,13 +16,18 @@ export PATH="/usr/local/go/bin:${GOPATH}/bin:${PATH}"
 
 cd ${GOPATH}/src/github.com/Netflix/titus-executor
 
+log "Installing go dependencies"
+
 go get -u github.com/kardianos/govendor
 go get -u github.com/wadey/gocovmerge
 go get -u github.com/mattn/goveralls
 
+log "Building executor"
 
 make clean
 make --output-sync -j16 builder all 2>&1 | tee build.log
+
+log "Running code coverage"
 
 bash <(curl -s https://codecov.io/bash)
 gocovmerge coverage-local.out coverage-standalone.out > coverage-combined.out
