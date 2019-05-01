e5c6424391acfb74a489f428bb859f2665583870
Sargun Dhillon <sargun@sargun.me>
2019-05-01 12:33:42 -0700

    Remove Logging to Journald only under systemd
    This makes it so that the executor doesn't rely on CGO. It needed
    cgo before to figure out if it was running as a systemd service
    or not. This makes it assume it will always run as a systemd service
    unless otherwise stated by console flags, rather than relying on
    environment detection.

--- logsutil/util_unsupported.go
+++ logsutil/util_unsupported.go
@@ -2,11 +2,6 @@
 
 package logsutil
 
-// MaybeSetupLoggerIfUnderSystemd sets up journald logging if running as a systemd unit
-func MaybeSetupLoggerIfUnderSystemd() {
-
-}
-
 // MaybeSetupLoggerIfOnJournaldAvailable sets up journald logging if the system
 func MaybeSetupLoggerIfOnJournaldAvailable() {
 }
