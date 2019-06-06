632acad58a90ff71a35347c85b63c7b0b20df1ed
Rob Gulewich <rgulewich@netflix.com>
2019-06-06 16:20:01 -0700

    Mount all nvidia libraries into the container, for backwards compatibility

--- nvidia/nvidia.go
+++ nvidia/nvidia.go
@@ -168,6 +168,9 @@ func (n *PluginInfo) UpdateContainerConfig(c *types.Container, dockerCfg *contai
 	// Now setup the environment variables that `nvidia-container-runtime` uses to configure itself,
 	// and remove any that may have been set by the user.  See https://github.com/NVIDIA/nvidia-container-runtime
 	c.Env["NVIDIA_VISIBLE_DEVICES"] = strings.Join(c.GPUInfo.Devices(), ",")
+	// nvidia-docker 1.0 would mount all of `/usr/local/nvidia/`, bringing in all of the shared libs.
+	// Setting this to "all" will mount all of those libs:
+	c.Env["NVIDIA_DRIVER_CAPABILITIES"] = "all"
 	dockerCfg.Env = c.GetSortedEnvArray()
 }
 
