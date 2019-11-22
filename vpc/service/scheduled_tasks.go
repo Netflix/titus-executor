8e95052fd1c813994d138c6af80317f412681eb2
Sargun Dhillon <sargun@sargun.me>
2019-11-22 10:20:02 -0800

    Fix scheduled task logic

--- vpc/service/scheduled_tasks.go
+++ vpc/service/scheduled_tasks.go
@@ -12,6 +12,8 @@ import (
 )
 
 func (vpcService *vpcService) runScheduledTask(ctx context.Context, taskName string, interval time.Duration, cb func(context.Context, *sql.Tx) error) (retErr error) {
+	ctx = logger.WithField(ctx, "taskName", taskName)
+	logger.G(ctx).Info("Planning to start task")
 	tx, err := vpcService.db.BeginTx(ctx, &sql.TxOptions{})
 	if err != nil {
 		logger.G(ctx).WithError(err).Error("Could not start database transaction")
@@ -33,7 +35,7 @@ func (vpcService *vpcService) runScheduledTask(ctx context.Context, taskName str
 		return err
 	}
 
-	queryRowContext := tx.QueryRowContext(ctx, "SELECT pg_try_advisory_lock(oid::int, scheduled_tasks.id) FROM scheduled_tasks, (SELECT oid FROM pg_class WHERE relname = 'scheduled_tasks') o WHERE name = $1", taskName)
+	queryRowContext := tx.QueryRowContext(ctx, "SELECT pg_try_advisory_xact_lock(oid::int, scheduled_tasks.id) FROM scheduled_tasks, (SELECT oid FROM pg_class WHERE relname = 'scheduled_tasks') o WHERE name = $1", taskName)
 	var hasLock bool
 	err = queryRowContext.Scan(&hasLock)
 	if err != nil {
@@ -42,6 +44,7 @@ func (vpcService *vpcService) runScheduledTask(ctx context.Context, taskName str
 	}
 
 	if !hasLock {
+		logger.G(ctx).Info("Skipping, as couldn't get lock")
 		return nil
 	}
 
@@ -53,7 +56,7 @@ func (vpcService *vpcService) runScheduledTask(ctx context.Context, taskName str
 		return err
 	}
 	if t := time.Since(lastRun); t < interval {
-		logger.G(ctx).WithField("t", t).Info("Aborting reconcilation, task ran too recently")
+		logger.G(ctx).WithField("t", t).Info("Aborting task ran too recently")
 		return nil
 	}
 
@@ -66,14 +69,14 @@ func (vpcService *vpcService) runScheduledTask(ctx context.Context, taskName str
 }
 
 func (vpcService *vpcService) taskLoop(ctx context.Context, interval time.Duration, taskPrefix string, cb func(context.Context, regionAccount, *sql.Tx) error) error {
-	t := time.NewTimer(interval)
+	t := time.NewTimer(interval / 10)
 	for {
 		select {
 		case <-ctx.Done():
 			return nil
 		case <-t.C:
 			_ = vpcService.runTask(ctx, interval, taskPrefix, cb)
-			t.Reset(interval)
+			t.Reset(interval / 10)
 		}
 	}
 }
@@ -82,7 +85,7 @@ func (vpcService *vpcService) runTask(ctx context.Context, interval time.Duratio
 	defer cancel()
 	ctx, span := trace.StartSpan(ctx, taskPrefix)
 	defer span.End()
-	logger.G(ctx).WithField("taskPrefix", taskPrefix).Info("Starting")
+	logger.G(ctx).WithField("taskPrefix", taskPrefix).Info("Starting task")
 	regionAccounts, err := vpcService.getRegionAccounts(ctx)
 	if err != nil {
 		logger.G(ctx).WithError(err).Error("Could not get region / accounts")
