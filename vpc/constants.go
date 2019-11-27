bf3f8f32cbac7a8490b9365b5d11e791c30466a1
Sargun Dhillon <sargun@sargun.me>
2019-11-26 19:00:58 -0800

    Remove unneccesary constants

--- vpc/constants.go
+++ vpc/constants.go
@@ -1,12 +1,6 @@
 package vpc
 
-import "time"
-
 const (
-	// RefreshInterval indicates how often an IP holder indicates that it holds it.
-	RefreshInterval = 30 * time.Second
-	// DefaultMinIdlePeriod is the minimum amount of time an IP must be idle before we consider it for GC
-	DefaultMinIdlePeriod = 90 * time.Second
 	// IngressIFB is the intermediate functional block device used to do ingress processing
 	IngressIFB = "ifb-ingress"
 	// EgressIFB is the intermediate functional block device used to do egress processing
