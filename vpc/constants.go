4d46491a52cd0757e3b35a02ab3cb7d352165ca9
Sargun Dhillon <sargun@sargun.me>
2019-11-26 16:45:05 -0800

    Bump IP Refresh interval to 30 seconds

--- vpc/constants.go
+++ vpc/constants.go
@@ -4,7 +4,7 @@ import "time"
 
 const (
 	// RefreshInterval indicates how often an IP holder indicates that it holds it.
-	RefreshInterval = 10 * time.Second
+	RefreshInterval = 30 * time.Second
 	// DefaultMinIdlePeriod is the minimum amount of time an IP must be idle before we consider it for GC
 	DefaultMinIdlePeriod = 90 * time.Second
 	// IngressIFB is the intermediate functional block device used to do ingress processing
