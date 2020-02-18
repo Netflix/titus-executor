3b76cb17c7381002129d9d56ce53d7f35fbfeec9
Manas Alekar <malekar@netflix.com>
2020-02-18 11:08:35 -0800

    Arrange imports.

--- executor/runner/runner.go
+++ executor/runner/runner.go
@@ -1,6 +1,8 @@
 package runner
 
 import (
+	"strconv"
+
 	"github.com/Netflix/metrics-client-go/metrics"
 	"github.com/Netflix/titus-executor/api/netflix/titus"
 	"github.com/Netflix/titus-executor/config"
@@ -13,7 +15,6 @@ import (
 	"github.com/Netflix/titus-executor/uploader"
 	"github.com/hashicorp/go-multierror"
 	"github.com/sirupsen/logrus"
-	"strconv"
 
 	"context"
 	"errors"
