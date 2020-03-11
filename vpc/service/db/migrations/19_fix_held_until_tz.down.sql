6a84664a0f8784664b021f18120aaa18c8b62c08
Sargun Dhillon <sargun@sargun.me>
2020-03-10 17:52:27 -0700

    Remove held_until (with tz) migration

--- vpc/service/db/migrations/19_fix_held_until_tz.down.sql
+++
@@ -1,3 +0,0 @@
-START TRANSACTION;
-alter table long_lived_locks alter column held_until type timestamp using held_until::timestamp;
-COMMIT;
--- vpc/service/db/migrations/19_fix_held_until_tz.up.sql
+++
@@ -1,3 +0,0 @@
-START TRANSACTION;
-alter table long_lived_locks alter column held_until type timestamptz using held_until::timestamptz;
-COMMIT;
