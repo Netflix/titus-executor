d98c91f2c2211c7862ec06a5648f5fafd42127aa
Sargun Dhillon <sargun@sargun.me>
2020-08-04 10:42:48 -0700

    Upgrade to go 1.13.14
    See: https://github.com/golang/go/issues/40205

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -5,7 +5,7 @@ log() {
     echo -e "[$(date -u +"%Y-%m-%dT%H:%M:%SZ")] $1" >&2
 }
 
-GO_VERSION=1.13.8
+GO_VERSION=1.13.14
 GO_INSTALL_DIR=${HOME}/go_installs/${GO_VERSION}
 if [[ ! -d ${GO_INSTALL_DIR}/go ]]; then
     mkdir -p ${GO_INSTALL_DIR}
