e199622aed744a853ecf17f4ddd72d5ba7fd3770
Kyle Anderson <kylea@netflix.com>
2020-10-02 17:24:04 +0000

    working mount outside a container with hacks

--- mount/mount.c
+++ mount/mount.c
@@ -18,6 +18,7 @@
 
 /* for mount syscalls */
 #include <asm-generic/unistd.h>
+#include <linux/mount.h>
 
 /* fcntl */
 #include <unistd.h>
@@ -58,8 +59,68 @@ static int dns_lookup(const char *hostname, struct sockaddr_in *addr)
 	return 0;
 }
 
-static int fsopen(const char *fsname, unsigned int flags) {
-	return syscall(__NR_fsopen, fsname, flags);
+#define E(x) do { if ((x) == -1) { perror(#x); exit(1); } } while(0)
+
+static void check_messages(int fd)
+{
+	char buf[4096];
+	int err, n;
+
+	err = errno;
+
+	for (;;) {
+		n = read(fd, buf, sizeof(buf));
+		if (n < 0)
+			break;
+		n -= 2;
+
+		switch (buf[0]) {
+		case 'e':
+			fprintf(stderr, "Error: %*.*s\n", n, n, buf + 2);
+			break;
+		case 'w':
+			fprintf(stderr, "Warning: %*.*s\n", n, n, buf + 2);
+			break;
+		case 'i':
+			fprintf(stderr, "Info: %*.*s\n", n, n, buf + 2);
+			break;
+		}
+	}
+
+	errno = err;
+}
+
+static __attribute__((noreturn))
+void mount_error(int fd, const char *s)
+{
+	check_messages(fd);
+	fprintf(stderr, "%s: %m\n", s);
+	exit(1);
+}
+
+static inline int fsopen(const char *fs_name, unsigned int flags)
+{
+	return syscall(__NR_fsopen, fs_name, flags);
+}
+
+static inline int fsmount(int fsfd, unsigned int flags, unsigned int ms_flags)
+{
+	return syscall(__NR_fsmount, fsfd, flags, ms_flags);
+}
+
+static inline int fsconfig(int fsfd, unsigned int cmd,
+			   const char *key, const void *val, int aux)
+{
+	return syscall(__NR_fsconfig, fsfd, cmd, key, val, aux);
+}
+
+static inline int move_mount(int from_dfd, const char *from_pathname,
+			     int to_dfd, const char *to_pathname,
+			     unsigned int flags)
+{
+	return syscall(__NR_move_mount,
+		       from_dfd, from_pathname,
+		       to_dfd, to_pathname, flags);
 }
 
 bool check_new_mount_api_available() {
@@ -67,15 +128,72 @@ bool check_new_mount_api_available() {
 	return errno != ENOSYS;
 }
 
-int do_fsmount(const char *source, const char *target, const char *fs_type, unsigned long flags_ul, const char *options) {
-	// TODO
-	return mount(source, target, fs_type, flags_ul, options);
-/*
-    int fsopen(const char *fsname, unsigned int flags);
-    int fsmount(int fd, unsigned int flags, unsigned int ms_flags);
-    int move_mount(int from_dfd, const char *from_path, int to_dfd, const char *to_path, unsigned int flags);
-*/
+#define E_fsconfig(fd, cmd, key, val, aux)                              \
+        do {                                                            \
+                if (fsconfig(fd, cmd, key, val, aux) == -1)             \
+                        mount_error(fd, key ?: "create");               \
+        } while (0)
 
+void do_fsconfigs(int fsfd, const char *options) {
+	// TODO
+	// echo "vers=4.1,nosharecache,rsize=1048576,wsize=1048576,timeo=600,retrans=2,addr=100.66.9.206,clientaddr=100.122.62.253,noresvport,fsc=d85f8159aec7" | tr ',' '\n' | sed 's/^/E_fsconfig(fsfd, FSCONFIG_SET_STRING, "/g' | sed 's/=/", "/g' | sed 's/$/", 0);/g'
+	E_fsconfig(fsfd, FSCONFIG_SET_STRING, "vers", "4.1", 0);
+//	E_fsconfig(fsfd, FSCONFIG_SET_STRING, "nosharecache", "", 0);
+//	E_fsconfig(fsfd, FSCONFIG_SET_STRING, "rsize", "1048576", 0);
+//	E_fsconfig(fsfd, FSCONFIG_SET_STRING, "wsize", "1048576", 0);
+//	E_fsconfig(fsfd, FSCONFIG_SET_STRING, "timeo", "600", 0);
+//	E_fsconfig(fsfd, FSCONFIG_SET_STRING, "retrans", "2", 0);
+	E_fsconfig(fsfd, FSCONFIG_SET_STRING, "addr", "100.66.9.206", 0);
+	E_fsconfig(fsfd, FSCONFIG_SET_STRING, "source", "fs-0208c74b.efs.us-east-1.amazonaws.com:.", 0);
+	E_fsconfig(fsfd, FSCONFIG_SET_STRING, "clientaddr", "100.122.62.253", 0);
+//	E_fsconfig(fsfd, FSCONFIG_SET_STRING, "noresvport", "",  0);
+//	E_fsconfig(fsfd, FSCONFIG_SET_STRING, "fsc", "d85f8159aec7", 0);
+	E_fsconfig(fsfd, FSCONFIG_CMD_CREATE, NULL, NULL, 0);
+//	E_fsconfig(fsfd, FSCONFIG_CMD_RECONFIGURE, NULL, NULL, 0);
+}
+
+
+int do_fsmount(const char *source, const char *target, const char *fs_type, unsigned long flags_ul, const char *options) {
+	int fsfd, mfd;
+
+	// Hard coding nfs4 here, the only type we currently support (note: not nfsv4)
+	fs_type = "nfs4";
+
+	// Legacy flags start at this MS_MGC_VAL. For the new mount calls we need to subtract it.
+	unsigned long flags;
+	if (flags_ul >= 0xC0ED0000) {
+		flags = flags_ul - 0xC0ED0000;
+	} else {
+		flags = flags_ul;
+	}
+
+	/* First we open a fd for holding the new fs */
+	printf("Running fsopen with %s with flags %lu\n", fs_type, flags);
+	fsfd = fsopen("nfs4", 0);
+	if (fsfd == -1) {
+                perror("fsopen");
+                exit(1);
+	}
+	
+	printf("Doing fdconfigs with these options: %s\n", options);
+	do_fsconfigs(fsfd, options);
+
+	/* Now we can fsmount it into a mount fd (mfd) */
+	printf("Calling fsmount with %d with args 0, 0\n", fsfd);
+	mfd = fsmount(fsfd, 0, MS_NODEV);
+        if (mfd < 0)
+                mount_error(fsfd, "fsmount");
+        E(close(fsfd));
+
+	/* Last we can move it to the target */
+	printf("Calling move_mount to %s\n", target);
+        if (move_mount(mfd, "", AT_FDCWD, target, MOVE_MOUNT_F_EMPTY_PATH) < 0) {
+                perror("move_mount");
+                exit(1);
+        }
+
+        E(close(mfd));
+	return 0;
 }
 
 int main() {
@@ -173,7 +291,7 @@ int main() {
 		fprintf(stderr, "titus-mount: using these nfs mount options: %s\n", final_options);
 	}
 
-	if (nfs_mount_hostname && check_new_mount_api_available()) {
+	if (check_new_mount_api_available()) {
 		/* Where possible, we try to use the newer fsmount syscalls for newer kernel support */
 		rc = do_fsmount(source, target, fs_type, flags_ul, final_options);
 	} else {
