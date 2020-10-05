4a7eba29c36451d2484f7c7c95a7073fea5377a9
Sargun Dhillon <sargun@sargun.me>
2020-10-05 11:05:15 -0700

    Refactor docker runtime to have an API which isn't designed around legacy mesos
    This change is mostly just cleanup. It's basically moving the Docker runtime
    logic to be purely Prepare/Start/Stop, and not getting into the idea that
    one given runner, or runtime can run multiple containers. This design
    originally stemmed from Mesos, where one executor would actuate multiple
    containers.
    
    We don't have Mesos anymore. :).

--- executor/runtime/docker/docker_linux.go
+++ executor/runtime/docker/docker_linux.go
@@ -142,19 +142,19 @@ func setupScheduler(cred ucred) error {
 }
 
 // This mounts /proc/${PID1}/ to /var/lib/titus-inits for the container
-func mountContainerProcPid1InTitusInits(parentCtx context.Context, c *runtimeTypes.Container, cred ucred) error {
+func (r *DockerRuntime) mountContainerProcPid1InTitusInits(parentCtx context.Context, c *runtimeTypes.Container, cred ucred) error {
 	pidpath := filepath.Join("/proc/", strconv.FormatInt(int64(cred.pid), 10))
 	path := filepath.Join(titusInits, c.TaskID)
 	if err := os.Mkdir(path, 0755); err != nil { // nolint: gosec
 		return err
 	}
-	c.RegisterRuntimeCleanup(func() error {
+	r.registerRuntimeCleanup(func() error {
 		return os.Remove(path)
 	})
 	if err := unix.Mount(pidpath, path, "", unix.MS_BIND, ""); err != nil {
 		return err
 	}
-	c.RegisterRuntimeCleanup(func() error {
+	r.registerRuntimeCleanup(func() error {
 		// 0x8 is
 		return unix.Unmount(path, unix.MNT_DETACH|umountNoFollow)
 	})
