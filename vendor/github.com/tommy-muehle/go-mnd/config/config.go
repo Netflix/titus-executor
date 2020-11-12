bd168504155abfad7dda37d72981a5700d8487e5
Sargun Dhillon <sargun@sargun.me>
2020-11-11 17:32:23 -0800

    Fix environment variable resolution
    I introduced a bug that allowed people to override the environment of Titus containers.
    There are certain environment variables that are special, and sould not be able
    to be overridden. These are set by the Titus executor.
    
    There is one special one where the control plane can set it --
    EC2_OWNER_ID.

--- vendor/github.com/tommy-muehle/go-mnd/config/config.go
+++ vendor/github.com/tommy-muehle/go-mnd/config/config.go
@@ -1,12 +1,14 @@
 package config
 
 import (
+	"regexp"
 	"strings"
 )
 
 type Config struct {
 	Checks         map[string]bool
 	IgnoredNumbers map[string]struct{}
+	Excludes       []*regexp.Regexp
 }
 
 type Option func(config *Config)
@@ -16,6 +18,10 @@ func DefaultConfig() *Config {
 		Checks: map[string]bool{},
 		IgnoredNumbers: map[string]struct{}{
 			"0": {},
+			"1": {},
+		},
+		Excludes: []*regexp.Regexp{
+			regexp.MustCompile(`_test.go`),
 		},
 	}
 }
@@ -26,6 +32,18 @@ func WithOptions(options ...Option) *Config {
 		option(c)
 	}
 	return c
+}
+
+func WithExcludes(excludes string) Option {
+	return func(config *Config) {
+		if excludes == "" {
+			return
+		}
+
+		for _, exclude := range strings.Split(excludes, ",") {
+			config.Excludes = append(config.Excludes, regexp.MustCompile(exclude))
+		}
+	}
 }
 
 func WithIgnoredNumbers(numbers string) Option {
