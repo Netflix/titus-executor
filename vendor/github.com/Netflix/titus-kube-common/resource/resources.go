736d3bced4c86635f35208440853c9d31e5b7ca2
Rob Gulewich <rgulewich@netflix.com>
2020-11-13 19:27:36 -0800

    VK should accept the legacy GPU resource name

--- vendor/github.com/Netflix/titus-kube-common/resource/resources.go
+++ vendor/github.com/Netflix/titus-kube-common/resource/resources.go
@@ -1,7 +1,14 @@
 package resource
 
 const (
-	ResourceNameGpu       = "titus/gpu"
-	ResourceNameNvidiaGpu = "nvidia.com/gpu"
-	ResourceNameNetwork   = "titus/network"
+	ResourceNameCpu           = "cpu"
+	ResourceNameGpu           = "titus/gpu"
+	ResourceNameNvidiaGpu     = "nvidia.com/gpu"
+	ResourceNameGpuLegacy     = "gpu"
+	ResourceNameMemory        = "memory"
+	ResourceNameNetwork       = "titus/network"
+	ResourceNameNetworkLegacy = "network"
+	ResourceNameDisk          = "ephemeral-storage"
+	ResourceNameDiskLegacy    = "storage"
+	ResourceNamePods          = "pods"
 )
