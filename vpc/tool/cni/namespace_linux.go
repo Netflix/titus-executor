6c3b46ba5633547479f991378d2651b3923d6988
Channing Conger <cconger@netflix.com>
2021-01-13 18:10:35 -0800

    Add hook to CNI to create symlink to netns mount
    We'll use this for making calls into the network namespace from the
    host.  The filename is to be a wellknown format that can be determined
    from a pod-name.

---
+++ vpc/tool/cni/namespace_linux.go
@@ -0,0 +1,40 @@
+// +build linux
+
+package cni
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+)
+
+const namespacePath string = "/var/run/pods/"
+
+func getNSAlias(podName string) string {
+	return filepath.Join(namespacePath, "netns-"+podName)
+}
+
+func createNetNSAlias(podName string, netnsPath string) error {
+	aliasPath := getNSAlias(podName)
+	err := os.Symlink(netnsPath, aliasPath)
+
+	if errors.Is(err, os.ErrNotExist) {
+		// Create base dir if missing
+		err := os.MkdirAll(filepath.Dir(aliasPath), 0755)
+		if err != nil {
+			return err
+		}
+		return os.Symlink(netnsPath, aliasPath)
+	}
+
+	return nil
+}
+
+func deleteNetNSAlias(podName string) error {
+	err := os.Remove(getNSAlias(podName))
+	if errors.Is(err, os.ErrNotExist) {
+		// No fail if the file is already gone
+		return nil
+	}
+	return err
+}
