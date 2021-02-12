69ab59cbe87c663bea9459662c47232702b108eb
Sargun Dhillon <sargun@sargun.me>
2021-02-12 00:14:55 -0800

    Update vendor/

--- vendor/github.com/Netflix/titus-kube-common/pod/labels.go
+++ vendor/github.com/Netflix/titus-kube-common/pod/labels.go
@@ -1,6 +1,7 @@
 package pod
 
 import (
+	"fmt"
 	"strconv"
 
 	corev1 "k8s.io/api/core/v1"
@@ -13,13 +14,22 @@ const (
 	DomainPod     = "pod.netflix.com"
 
 	// Job details
-	LabelKeyApp      = "netflix.com/applicationName"
-	LabelKeyDetail   = "netflix.com/detail"
-	LabelKeySequence = "netflix.com/sequence"
-	LabelKeyStack    = "netflix.com/stack"
+	LabelKeyAppLegacy      = "netflix.com/applicationName"
+	LabelKeyDetailLegacy   = "netflix.com/detail"
+	LabelKeySequenceLegacy = "netflix.com/sequence"
+	LabelKeyStackLegacy    = "netflix.com/stack"
 
-	LabelKeyByteUnitsEnabled = "pod.titus.netflix.com/byteUnits"
-	LabelKeyCapacityGroup    = "titus.netflix.com/capacityGroup"
+	LabelKeyByteUnitsEnabled    = "pod.titus.netflix.com/byteUnits"
+	LabelKeyCapacityGroupLegacy = "titus.netflix.com/capacityGroup"
+
+	// v1 pod labels
+	LabelKeyJobId         = "v3.job.titus.netflix.com/job-id"
+	LabelKeyTaskId        = "v3.job.titus.netflix.com/task-id"
+	LabelKeyAppName       = "app.netflix.com/name"
+	LabelKeyAppStack      = "app.netflix.com/stack"
+	LabelKeyAppDetail     = "app.netflix.com/detail"
+	LabelKeyAppSequence   = "app.netflix.com/sequence"
+	LabelKeyCapacityGroup = "titus.netflix.com/capacity-group"
 )
 
 // Is the control plane indicating that it's sending the resources in bytes?
@@ -30,4 +40,31 @@ func ByteUnitsEnabled(pod *corev1.Pod) (bool, error) {
 	}
 
 	return strconv.ParseBool(bytesEnabled)
+}
+
+func parseLabels(pod *corev1.Pod, pConf *Config) error {
+	labels := pod.GetLabels()
+
+	// Only parse the labels that aren't duplicates of annotations
+	cVal, ok := labels[LabelKeyCapacityGroup]
+	if ok {
+		pConf.CapacityGroup = &cVal
+	}
+
+	// Maybe pull this from the containers in the pod instead?
+	tVal, ok := labels[LabelKeyTaskId]
+	if ok {
+		pConf.TaskID = &tVal
+	}
+
+	bytesEnabledStr, ok := pod.GetLabels()[LabelKeyByteUnitsEnabled]
+	if ok {
+		val, err := strconv.ParseBool(bytesEnabledStr)
+		if err != nil {
+			return fmt.Errorf("label is not a valid boolean value: %s", LabelKeyByteUnitsEnabled)
+		}
+		pConf.BytesEnabled = &val
+	}
+
+	return nil
 }
