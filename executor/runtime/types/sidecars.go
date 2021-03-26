d16dff74f7570d022711efb12c6e84202e402b48
Kyle Anderson <kylea@netflix.com>
2021-03-25 19:02:35 -0700

    Move the 'shouldStart' function into types

--- executor/runtime/types/sidecars.go
+++ executor/runtime/types/sidecars.go
@@ -53,4 +53,84 @@ var sideCars = []SidecarContainerConfig{
 			"/titus/atlas-titus-agent": {},
 		},
 	},
+	SidecarSeccompAgent: {
+		UnitName:     "titus-sidecar-seccomp-agent",
+		Required:     true,
+		EnabledCheck: shouldStartTitusSeccompAgent,
+	},
+	SidecarTitusStorage: {
+		UnitName:     "titus-sidecar-storage",
+		Required:     true,
+		EnabledCheck: shouldStartTitusStorage,
+	},
+}
+
+func shouldStartMetatronSync(cfg *config.Config, c Container) bool {
+	if cfg.MetatronEnabled && c.MetatronCreds() != nil {
+		return true
+	}
+
+	return false
+}
+
+func shouldStartTitusSeccompAgent(cfg *config.Config, c Container) bool {
+	return c.SeccompAgentEnabledForPerfSyscalls() || c.SeccompAgentEnabledForNetSyscalls()
+}
+
+func shouldStartServiceMesh(cfg *config.Config, c Container) bool {
+	return c.ServiceMeshEnabled()
+}
+
+func shouldStartAbmetrix(cfg *config.Config, c Container) bool {
+	enabled := cfg.ContainerAbmetrixEnabled
+	if !enabled {
+		return false
+	}
+
+	if cfg.AbmetrixServiceImage == "" {
+		return false
+	}
+	return true
+
+}
+
+func shouldStartSpectatord(cfg *config.Config, c Container) bool {
+	enabled := cfg.ContainerSpectatord
+	if !enabled {
+		return false
+	}
+
+	if cfg.SpectatordServiceImage == "" {
+		return false
+	}
+	return true
+}
+
+func shouldStartAtlasd(cfg *config.Config, c Container) bool {
+	enabled := cfg.ContainerAtlasd
+	if !enabled {
+		return false
+	}
+
+	if cfg.AtlasdServiceImage == "" {
+		return false
+	}
+	return true
+}
+
+// This starts the old version of the atlas titus agent, which we are migrating to a system service.
+func shouldStartAtlasAgent(cfg *config.Config, c Container) bool {
+	return !shouldStartAtlasd(cfg, c)
+}
+
+func shouldStartSSHD(cfg *config.Config, c Container) bool {
+	return cfg.ContainerSSHD
+}
+
+func shouldStartLogViewer(cfg *config.Config, c Container) bool {
+	return cfg.ContainerLogViewer
+}
+
+func shouldStartTitusStorage(cfg *config.Config, c Container) bool {
+	return c.EBSInfo().VolumeID != ""
 }
