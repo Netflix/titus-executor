a41d3f962a7b94788d1d2380464b1efc94a51b73
Sargun Dhillon <sargun@sargun.me>
2021-08-17 14:29:55 -0700

    Tie logging back to span ID

--- vpc/service/assign_addresses_v3.go
+++ vpc/service/assign_addresses_v3.go
@@ -805,7 +805,7 @@ func (vpcService *vpcService) assignIPsToENI(ctx context.Context, req *vpcapi.As
 	}
 
 	if !hasSecurityGroupSet.Equal(sets.NewString(req.SecurityGroupIds...)) {
-		logger.G(ctx).Warnf("Branch ENI has security groups %s, when expected %s", hasSecurityGroupSet.List(), req.SecurityGroupIds)
+		logger.G(ctx).WithField("spanid", span.SpanContext().SpanID.String()).Warnf("Branch ENI has security groups %s, when expected %s", hasSecurityGroupSet.List(), req.SecurityGroupIds)
 	}
 
 	/*
