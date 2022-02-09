13bdc491dfd5b6a904375bb150174d2933d87efb
Kyle Anderson <kylea@netflix.com>
2022-02-09 15:22:24 -0800

    Added basic seccomp tests

---
+++ executor/runtime/docker/seccomp/seccomp_test.go
@@ -0,0 +1,31 @@
+package seccomp
+
+import (
+	"testing"
+
+	dockerSeccomp "github.com/docker/docker/profiles/seccomp"
+	"github.com/opencontainers/runtime-spec/specs-go"
+	"github.com/stretchr/testify/assert"
+)
+
+func getGenericDockerSpec() specs.Spec {
+	rs := specs.Spec{}
+	rs.Process = &specs.Process{}
+	rs.Process.Capabilities = &specs.LinuxCapabilities{}
+	rs.Process.Capabilities.Bounding = []string{}
+	return rs
+}
+
+func TestAllProfilesLoad(t *testing.T) {
+	rs := getGenericDockerSpec()
+	for _, name := range AssetNames() {
+		a, err := Asset(name)
+		assert.NoError(t, err)
+		// The "json files" (compiled into seccomp bin data) are a docker-specific
+		// format, so we must use docker's seccomp profile loading library to ensure
+		// they are valid.
+		profile, err := dockerSeccomp.LoadProfile(string(a), &rs)
+		assert.NoError(t, err)
+		assert.NotNil(t, profile)
+	}
+}
