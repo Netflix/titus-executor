7f933f7261367153e4124ac71054fcf8003c6194
Sargun Dhillon <sargun@sargun.me>
2022-03-10 12:47:26 -0800

    Fix assignment of transition namespaces in VPC Tool

--- vpc/tool/assign3/assign_network.go
+++ vpc/tool/assign3/assign_network.go
@@ -186,7 +186,7 @@ func doAllocateNetwork(ctx context.Context, instanceIdentityProvider identity.In
 	} else if args.NetworkMode == titus.NetworkConfiguration_Ipv6AndIpv4Fallback.String() || args.NetworkMode == titus.NetworkConfiguration_HighScale.String() {
 		assignIPRequest.Ipv4 = &vpcapi.AssignIPRequestV3_TransitionRequested{}
 	} else {
-		logger.G(ctx).WithField("assignIPRequest", assignIPRequest).Debug("Experimental: Not assigning IPv4")
+		logger.G(ctx).WithField("assignIPRequest", assignIPRequest).Warning("Experimental: Not assigning IPv4")
 	}
 
 	logger.G(ctx).WithField("assignIPRequest", assignIPRequest).Debug("Making assign IP request")
