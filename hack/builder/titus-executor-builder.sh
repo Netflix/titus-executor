fba7d99d561d5a4981df665ffcb1efc7559a33d6
Kyle Anderson <kylea@netflix.com>
2022-08-22 13:24:19 -0700

    Use nfpm instead of fpm for packaging
    Ruby kinda sucks. nfpm is more self-contained.
    
    Here is the info of a deb created this way:
    
    ```
     new Debian package, version 2.0.
     size 141167754 bytes: control archive=2153 bytes.
           1 bytes,     0 lines      conffiles
         439 bytes,    11 lines      control
        4382 bytes,    55 lines      md5sums
         179 bytes,     4 lines   *  postinst             #!/bin/bash
     Package: titus-executor
     Version: 20210319.0.0~922-g295e3e86
     Section: default
     Priority: optional
     Architecture: amd64
     Maintainer: titus-developers@netflix.com
     Installed-Size: 314583
     Depends: libc6, apparmor >= 2.12
     Recommends: docker-ce >= 5:18.09.1~3-0~ubuntu-xenial, lxcfs, atlas-titus-agent, nvidia-container-runtime-hook
     Homepage: https://github.com/Netflix/titus-executor
     Description: titus-executor is spawned by titus-virtual-kubelet
    ```
    
    There is some loss here, but nothing major.

--- hack/builder/titus-executor-builder.sh
+++ hack/builder/titus-executor-builder.sh
@@ -35,88 +35,32 @@ install -t root/apps/titus-executor/bin build/bin/linux-amd64/*
 
 
 ## Setup the environment
-
 outdir="$(mktemp -d)"
-git_sha=$(git rev-parse --verify HEAD)
-git_sha_short=${git_sha:0:8}
-version=${version:-$(git describe --tags --long)}
-iteration="--iteration ${ITERATION:-$(date +%s)}"
+export git_sha=$(git rev-parse --verify HEAD)
+export git_sha_short=${git_sha:0:8}
+export version=${version:-$(git describe --tags --long)}
+export iteration="--iteration ${ITERATION:-$(date +%s)}"
 
 # when on CI/Jenkins
 if [[ -n "${BUILD_NUMBER:-}" ]]; then
     last_tag=$(git describe --abbrev=0 --tags | sed 's/^[a-zA-Z]//')
-    version="${last_tag}-h${BUILD_NUMBER}.${git_sha_short}"
+    export version="${last_tag}-h${BUILD_NUMBER}.${git_sha_short}"
     unset iteration
 fi
 
-# Build a tarball
-mkdir -p build/tarball
-install -t build/tarball build/bin/linux-amd64/*
-tar  -czv -C build/tarball -f ${outdir}/titus-executor-${version}.tar.gz .
-
 ## Build the deb package
-
-MAYBE_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-if [[ ${BUILDKITE_BRANCH:-$MAYBE_BRANCH} != "master" && "${ENABLE_DEV:-true}" == "true" ]]; then
-    provides="--provides titus-executor-dev"
-fi
-
-cat <<-EOF >/tmp/post-install.sh
-#!/bin/bash
-systemctl --system daemon-reload
-# TODO(Sargun): Make this reload apparmor only if apparmor is "started"
-systemctl reload apparmor || echo "Could not reload apparmor"
-EOF
-chmod +x /tmp/post-install.sh
-
-fpm -t deb -s dir -C root \
-  -a amd64 \
-  -n titus-executor \
-  --maintainer titus-developers@netflix.com \
-  ${iteration:-} \
-  --version "$version" \
-  --deb-field "Build-Host: ${BUILD_HOST:-}" \
-  --deb-field "Build-Job: ${BUILD_JOB:-}" \
-  --deb-field "Build-Number: ${BUILD_NUMBER:-}" \
-  --deb-field "Build-Id: ${BUILD_ID:-}" \
-  --deb-field "Implementation-Vendor: Netflix, Inc." \
-  --deb-field "Built-By: fpm" \
-  --deb-field "Built-OS: Linux" \
-  --deb-field "Build-Date: $(date -u +"%Y-%m-%d_%H:%M:%S")" \
-  --deb-field "Module-Owner: titus-developers@netflix.com" \
-  --deb-field "Module-Email: titus-developers@netflix.com" \
-  --deb-field "Module-Origin: ssh://git@github.com:Netflix/titus-executor.git" \
-  --deb-field "Change: ${git_sha_short}" \
-  --deb-field "Branch: ${git_sha}" \
-  --deb-activate ldconfig \
-  --depends libc6 \
-  --depends 'apparmor >= 2.12' \
-  --deb-recommends 'docker-ce >= 5:18.09.1~3-0~ubuntu-xenial' \
-  --deb-recommends lxcfs \
-  --deb-recommends atlas-titus-agent \
-  --deb-recommends nvidia-container-runtime-hook \
-  ${provides:-} \
-  --after-install /tmp/post-install.sh \
-  --package "${outdir}/"
-
+export BUILD_DATE=$(date -u +"%Y-%m-%d_%H:%M:%S")
+nfpm package --target $outdir --packager deb
 num_debs=$(find "$outdir" -iname "*.deb" | wc -l)
 if [[ $num_debs -ne 1 ]]; then
     echo "Expected exactly one deb file in ${outdir}" >&2
     ls "$outdir" >&2
     exit 1
 fi
-
 filename=$(ls -t ${outdir}/*.deb | grep -v latest | head -n 1)
-
-# TODO: only run the linter on the file above
-# see: nebula/src/main/groovy/netflix/nebula/ospackage/NebulaOsPackageDebRepositoryPublish.groovy
-lintian --suppress-tags dir-or-file-in-opt,statically-linked-binary,unstripped-binary-or-object,debian-changelog-file-missing-or-wrong-name,no-copyright-file,extended-description-is-empty,python-script-but-no-python-dep,non-standard-toplevel-dir,maintainer-name-missing,maintainer-address-malformed,maintainer-script-should-not-use-adduser-system-without-home \
-  --no-tag-display-limit ${filename}
 
 mkdir -p build/distributions
 mv ${filename} build/distributions
-mv ${outdir}/titus-executor-${version}.tar.gz build/distributions
 
 echo "## Updating the symlink: titus-executor_latest.deb -> ${filename}" >&2
 pushd build/distributions
