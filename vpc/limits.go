165748c4645f5a83fb1088a7d4f7f3784f0eb1f8
Kyle Anderson <kylea@netflix.com>
2022-11-08 14:33:55 -0800

    Simplified vpc limits lookup to just use instance type instead of family

--- vpc/limits.go
+++ vpc/limits.go
@@ -2,7 +2,6 @@ package vpc // nolint:dupl
 
 import (
 	"fmt"
-	"strings"
 )
 
 type limits struct {
@@ -14,204 +13,184 @@ type limits struct {
 }
 
 // Interfaces, and IPs per interface comes from https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-eni.html#AvailableIpPerENI
-var interfaceLimits = map[string]map[string]limits{
-	"p4d": {
-		"24xlarge": limits{
-			interfaces:              15,
-			ipAddressesPerInterface: 50,
-			networkThroughput:       400000,
-			branchENIs:              120,
-		},
+var interfaceLimits = map[string]limits{
+	"p4d.24xlarge": {
+		interfaces:              15,
+		ipAddressesPerInterface: 50,
+		networkThroughput:       400000,
+		branchENIs:              120,
 	},
-	"g4dn": {
-		"xlarge": limits{
-			interfaces:              3,
-			ipAddressesPerInterface: 10,
-			networkThroughput:       5000,
-			branchENIs:              10,
-		},
-		"2xlarge": limits{
-			interfaces:              3,
-			ipAddressesPerInterface: 10,
-			networkThroughput:       10000,
-			branchENIs:              20,
-		},
-		"4xlarge": limits{
-			interfaces:              3,
-			ipAddressesPerInterface: 10,
-			networkThroughput:       20000,
-			branchENIs:              60,
-		},
-		"8xlarge": limits{
-			interfaces:              4,
-			ipAddressesPerInterface: 15,
-			networkThroughput:       50000,
-			branchENIs:              60,
-		},
-		"12xlarge": limits{
-			interfaces:              8,
-			ipAddressesPerInterface: 30,
-			networkThroughput:       50000,
-			branchENIs:              60,
-		},
-		"16xlarge": limits{
-			interfaces:              15,
-			ipAddressesPerInterface: 50,
-			networkThroughput:       50000,
-			branchENIs:              120,
-		},
-		"metal": limits{
-			interfaces:              15,
-			ipAddressesPerInterface: 50,
-			networkThroughput:       100000,
-			branchENIs:              120,
-		},
+	"g4dn.xlarge": {
+		interfaces:              3,
+		ipAddressesPerInterface: 10,
+		networkThroughput:       5000,
+		branchENIs:              10,
 	},
-	"g5": {
-		"48xlarge": limits{
-			interfaces:              15,
-			ipAddressesPerInterface: 50,
-			networkThroughput:       100000,
-			branchENIs:              120,
-		},
+	"g4dn.2xlarge": {
+		interfaces:              3,
+		ipAddressesPerInterface: 10,
+		networkThroughput:       10000,
+		branchENIs:              20,
 	},
-	"m5": {
-		"large": limits{
-			interfaces:              3,
-			ipAddressesPerInterface: 10,
-			networkThroughput:       100,
-			branchENIs:              10,
-		},
-		"xlarge": limits{
-			interfaces:              4,
-			ipAddressesPerInterface: 15,
-			networkThroughput:       1000,
-			branchENIs:              20,
-		},
-		"2xlarge": limits{
-			interfaces:              4,
-			ipAddressesPerInterface: 15,
-			networkThroughput:       1000,
-			branchENIs:              40,
-		},
-		"4xlarge": limits{
-			interfaces:              8,
-			ipAddressesPerInterface: 30,
-			networkThroughput:       2000,
-			branchENIs:              60,
-		},
-		"12xlarge": limits{
-			interfaces:              8,
-			ipAddressesPerInterface: 30,
-			// Is this number correct?
-			networkThroughput: 10000,
-			branchENIs:        60,
-		},
-		"24xlarge": limits{
-			interfaces:              15,
-			ipAddressesPerInterface: 50,
-			networkThroughput:       23000,
-			branchENIs:              120,
-		},
-		"metal": limits{
-			interfaces:              12,
-			ipAddressesPerInterface: 50,
-			networkThroughput:       25000,
-			branchENIs:              198,
-		},
+	"g4dn.4xlarge": {
+		interfaces:              3,
+		ipAddressesPerInterface: 10,
+		networkThroughput:       20000,
+		branchENIs:              60,
 	},
-	"r5": {
-		"large": limits{
-			interfaces:              3,
-			ipAddressesPerInterface: 10,
-			networkThroughput:       1000,
-			branchENIs:              10,
-		},
-		"xlarge": limits{
-			interfaces:              4,
-			ipAddressesPerInterface: 15,
-			networkThroughput:       1000,
-			branchENIs:              20,
-		},
-		"2xlarge": limits{
-			interfaces:              4,
-			ipAddressesPerInterface: 15,
-			networkThroughput:       2000,
-			branchENIs:              40,
-		},
-		"4xlarge": limits{
-			interfaces:              8,
-			ipAddressesPerInterface: 30,
-			networkThroughput:       4000,
-			branchENIs:              60,
-		},
-		"12xlarge": limits{
-			interfaces:              8,
-			ipAddressesPerInterface: 30,
-			networkThroughput:       9000,
-			branchENIs:              60,
-		},
-		"24xlarge": limits{
-			interfaces:              15,
-			ipAddressesPerInterface: 50,
-			networkThroughput:       23000,
-			branchENIs:              120,
-		},
-		"metal": limits{
-			interfaces:              12,
-			ipAddressesPerInterface: 50,
-			networkThroughput:       25000,
-			branchENIs:              198,
-		},
+	"g4dn.8xlarge": {
+		interfaces:              4,
+		ipAddressesPerInterface: 15,
+		networkThroughput:       50000,
+		branchENIs:              60,
 	},
-	"c5": {
-		"large": limits{
-			interfaces:              3,
-			ipAddressesPerInterface: 10,
-			// Maybe?
-			networkThroughput: 1000,
-		},
-		"xlarge": limits{
-			interfaces:              4,
-			ipAddressesPerInterface: 15,
-			networkThroughput:       2000,
-		},
-		"2xlarge": limits{
-			interfaces:              4,
-			ipAddressesPerInterface: 15,
-			networkThroughput:       2000,
-		},
-		"4xlarge": limits{
-			interfaces:              4,
-			ipAddressesPerInterface: 30,
-			networkThroughput:       4000,
-		},
-		"9xlarge": limits{
-			interfaces:              4,
-			ipAddressesPerInterface: 30,
-			networkThroughput:       10000,
-		},
-		"18xlarge": limits{
-			interfaces:              4,
-			ipAddressesPerInterface: 50,
-			networkThroughput:       23000,
-		},
+	"g4dn.12xlarge": {
+		interfaces:              8,
+		ipAddressesPerInterface: 30,
+		networkThroughput:       50000,
+		branchENIs:              60,
+	},
+	"g4dn.16xlarge": {
+		interfaces:              15,
+		ipAddressesPerInterface: 50,
+		networkThroughput:       50000,
+		branchENIs:              120,
+	},
+	"g4dn.metal": {
+		interfaces:              15,
+		ipAddressesPerInterface: 50,
+		networkThroughput:       100000,
+		branchENIs:              120,
+	},
+	"g5.48xlarge": {
+		interfaces:              15,
+		ipAddressesPerInterface: 50,
+		networkThroughput:       100000,
+		branchENIs:              120,
+	},
+	"m5.large": {
+		interfaces:              3,
+		ipAddressesPerInterface: 10,
+		networkThroughput:       100,
+		branchENIs:              10,
+	},
+	"m5.xlarge": {
+		interfaces:              4,
+		ipAddressesPerInterface: 15,
+		networkThroughput:       1000,
+		branchENIs:              20,
+	},
+	"m5.2xlarge": {
+		interfaces:              4,
+		ipAddressesPerInterface: 15,
+		networkThroughput:       1000,
+		branchENIs:              40,
+	},
+	"m5.4xlarge": {
+		interfaces:              8,
+		ipAddressesPerInterface: 30,
+		networkThroughput:       2000,
+		branchENIs:              60,
+	},
+	"m5.12xlarge": {
+		interfaces:              8,
+		ipAddressesPerInterface: 30,
+		// Is this number correct?
+		networkThroughput: 10000,
+		branchENIs:        60,
+	},
+	"m5.24xlarge": {
+		interfaces:              15,
+		ipAddressesPerInterface: 50,
+		networkThroughput:       23000,
+		branchENIs:              120,
+	},
+	"m5.metal": {
+		interfaces:              12,
+		ipAddressesPerInterface: 50,
+		networkThroughput:       25000,
+		branchENIs:              198,
+	},
+	"r5.large": {
+		interfaces:              3,
+		ipAddressesPerInterface: 10,
+		networkThroughput:       1000,
+		branchENIs:              10,
+	},
+	"r5.xlarge": {
+		interfaces:              4,
+		ipAddressesPerInterface: 15,
+		networkThroughput:       1000,
+		branchENIs:              20,
+	},
+	"r5.2xlarge": {
+		interfaces:              4,
+		ipAddressesPerInterface: 15,
+		networkThroughput:       2000,
+		branchENIs:              40,
+	},
+	"r5.4xlarge": {
+		interfaces:              8,
+		ipAddressesPerInterface: 30,
+		networkThroughput:       4000,
+		branchENIs:              60,
+	},
+	"r5.12xlarge": {
+		interfaces:              8,
+		ipAddressesPerInterface: 30,
+		networkThroughput:       9000,
+		branchENIs:              60,
+	},
+	"r5.24xlarge": {
+		interfaces:              15,
+		ipAddressesPerInterface: 50,
+		networkThroughput:       23000,
+		branchENIs:              120,
+	},
+	"r5.metal": {
+		interfaces:              12,
+		ipAddressesPerInterface: 50,
+		networkThroughput:       25000,
+		branchENIs:              198,
+	},
+	"c5.large": {
+		interfaces:              3,
+		ipAddressesPerInterface: 10,
+		// Maybe?
+		networkThroughput: 1000,
+	},
+	"c5.xlarge": {
+		interfaces:              4,
+		ipAddressesPerInterface: 15,
+		networkThroughput:       2000,
+	},
+	"c5.2xlarge": {
+		interfaces:              4,
+		ipAddressesPerInterface: 15,
+		networkThroughput:       2000,
+	},
+	"c5.4xlarge": {
+		interfaces:              4,
+		ipAddressesPerInterface: 30,
+		networkThroughput:       4000,
+	},
+	"c5.9xlarge": {
+		interfaces:              4,
+		ipAddressesPerInterface: 30,
+		networkThroughput:       10000,
+	},
+	"c5.18xlarge": {
+		interfaces:              4,
+		ipAddressesPerInterface: 50,
+		networkThroughput:       23000,
 	},
 }
 
 // This function will panic if the instance type is unknown
 func getLimits(instanceType string) (limits, error) {
-	familyAndSubtype := strings.Split(instanceType, ".")
-	family := familyAndSubtype[0]
-	subtype := familyAndSubtype[1]
-	subtypes, ok := interfaceLimits[family]
+	l, ok := interfaceLimits[instanceType]
 	if !ok {
-		return limits{}, fmt.Errorf("Unknown family: %q", family)
-	}
-
-	l, ok := subtypes[subtype]
-	if !ok {
-		return limits{}, fmt.Errorf("Unknown subtype %q for family: %q", subtype, family)
+		return limits{}, fmt.Errorf("Unknown instance type: %q", instanceType)
 	}
 	return l, nil
 }
@@ -228,28 +207,24 @@ func mustGetLimits(instanceType string) limits {
 // includes the primary ENI
 func GetMaxInterfaces(instanceType string) (int, error) {
 	l, err := getLimits(instanceType)
-
 	return l.interfaces, err
 }
 
 // GetMaxIPAddresses returns  the maximum number of IPv4 addresses that this instance type can handle
 func GetMaxIPAddresses(instanceType string) (int, error) {
 	l, err := getLimits(instanceType)
-
 	return l.ipAddressesPerInterface, err
 }
 
 // GetMaxNetworkMbps returns the maximum network throughput in Megabits per second that this instance type can handle
 func GetMaxNetworkMbps(instanceType string) (int, error) {
 	l, err := getLimits(instanceType)
-
 	return l.networkThroughput, err
 }
 
 // GetMaxNetworkbps returns the maximum network throughput in bits per second that this instance type can handle
 func GetMaxNetworkbps(instanceType string) (uint64, error) {
 	maxNetworkMbps, err := GetMaxNetworkMbps(instanceType)
-
 	return uint64(maxNetworkMbps) * 1000 * 1000, err
 }
 
